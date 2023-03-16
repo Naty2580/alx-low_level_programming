41d458d9be0f95e839822d987e3df6982be20a41
Naty2580 <natyyo2580@gmaill.com>
2023-03-16 11:25:51 -0700

    print given word with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * puts(str) - prints te word assigned to it
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{ 
+{
 	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
+
+	puts (str);
 	return (0);
 }
