f96319fcdc1131fb69d9acf1da95530be5a36ac8
Naty2580 <natyyo2580@gmaill.com>
2023-03-16 12:56:50 -0700

    print given data using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prinf Return-the given text in the parenthesis
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
